0b705f17a43c782c1d777b6ac66a086d0bd0df61
Gustavo Soares <gustavosoaresoliveira@usp.br>
2024-07-10 15:56:28 -0300

    Database upload

--- my-backend/src/app.module.ts
+++ my-backend/src/app.module.ts
@@ -1,17 +1,38 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { UploadModule } from './upload/upload.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { S3 } from 'aws-sdk';
+import { S3StorageService } from './s3-storage.service'; // Serviço para S3
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true, // Apenas para desenvolvimento
+      }),
+      inject: [ConfigService],
     }),
-    UploadModule,
-    PrismaModule,
+    MulterModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        storage: multerS3({
+          s3: new S3(),
+          bucket: configService.get<string>('AWS_S3_BUCKET_NAME'),
+          acl: 'public-read',
+          key: (req, file, cb) => {
+            cb(null, `${Date.now().toString()}-${file.originalname}`);
+          },
+        }),
+      }),
+      inject: [ConfigService],
+    }),
   ],
-  controllers: [],
-  providers: [],
+  providers: [S3StorageService],
 })
 export class AppModule { }
